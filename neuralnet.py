7481222c84c872298a01bff88c690b0fc7af1513
Sophia Leeman <Sophia.Leeman@duke.edu>
2022-05-10 17:01:26 -0400

    Editing reading of csv

--- neuralnet.py
+++ neuralnet.py
@@ -5,9 +5,11 @@ import csv
 #Read csv
 file = open("trainingdata.csv")
 filereader = csv.reader(file)
-rows = []
+inputs = []
+outputs = []
 for row in filereader:
-    rows.append(row)
+    inputs.append(row[0:len(row)])
+    outputs.append(row[len(row)])
 file.close()
 
 #Define NeuralNet class
@@ -18,12 +20,13 @@ class NeuralNet:
         np.random.seed(1)
         #Initialize weight to a random value in the range -1 to 1
         self.weight = 2 * np.random.random((columns,1)) - 1 
-    def __calc_output(self, traindata):
+    def __calc_output(self, data):
         #Calculate weighted sum
-        weight_sum = np.dot(traindata, self.weight)
+        weight_sum = np.dot(data, self.weight)
         #Sigmoid function normalizes result to lie between 0 and 1
         return 1 / (1 + np.exp(weight_sum * -1))
-    
+    def __train(self, train_output, train_input, iterations):
+        
         
         
         
